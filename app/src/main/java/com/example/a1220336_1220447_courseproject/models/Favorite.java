59df8219fd69f218afeb505706e2037b2e6f140f
DuaaNaji <duaanaji2020@gmail.com>
2026-06-15 20:43:12 +0300

    Added models and DatabaseHelper

--- app/src/main/java/com/example/a1220336_1220447_courseproject/models/Favorite.java
+++ app/src/main/java/com/example/a1220336_1220447_courseproject/models/Favorite.java
@@ -1,4 +1,24 @@
 package com.example.a1220336_1220447_courseproject.models;
 
 public class Favorite {
+    private int id;
+    private int userId;
+    private int eventId;
+
+    public Favorite() {}
+
+    public Favorite(int id, int userId, int eventId) {
+        this.id = id;
+        this.userId = userId;
+        this.eventId = eventId;
+    }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public int getUserId() { return userId; }
+    public void setUserId(int userId) { this.userId = userId; }
+
+    public int getEventId() { return eventId; }
+    public void setEventId(int eventId) { this.eventId = eventId; }
 }
